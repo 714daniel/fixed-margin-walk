2b13d94c5989024ab7e26e64ee2d1cb762bf8479
Daniel Smith <714.daniel.smith@gmail.com>
2021-04-23 10:18:36 -0700

    Added test set of matrices

--- a_rc_walk.py
+++ a_rc_walk.py
@@ -1,34 +1,44 @@
-from random import randint, sample
+from random import randint, sample, uniform
+from copy import deepcopy 
+from timeit import default_timer as timer
+import numpy as np
+import matplotlib.pyplot as plt
+import re, threading
+from multiprocessing import Pool
 
+#UPPER_BOUND = 10
+BURN_IN_TIME = 2500
+SAMPLE_SIZE = 200
+STEP_SIZE = 100
+USE_INDEPENDENT_SAMPLES = True
+K = 0
+CONC_PROC = 8
 
-UPPER_BOUND = 5
-def gen_all_mat(m=3, n = 3, r=3, c=3):
-    matrices = set()
-    
-    for rep in range(10000):
-        curMat = [None] * n
-        for rows in range(n):
-            row = None
-            if randint(0,1) == 0:
-                row = 1,1,1,
-            else:
-                row = tuple(sample([0,1,2], n))
-                #print(row)
-            curMat[rows] = row
-        #print(curMat)
-        #exit()
-        valid = True
-        for row in range(n):
-            if curMat[0][row] + curMat[1][row] + curMat[2][row] != c:
-                valid = False
-        if valid:
-            matrices.add(tuple(curMat))
-    return matrices
+def memoize(fn):
+    """Creates a new version of fn that caches previous values."""
+    return fn
+    cache = { }
+    def memoized_function(*args):
+        cur_state = args[0]
+        if type(cur_state) in [list,tuple, np.ndarray] and type(cur_state[0]) in [list,tuple, np.ndarray]:
+            key = tuple(tuple(i) for i in cur_state)
+            if key not in cache:
+                cache[key] = fn(*args)
+        else:
+            key = tuple(args)
+            if key not in cache:
+                cache[key] = fn(*key)
+            #else: 
+            #    print('CACHE HIT FOR FN ', fn.__name__)
+        return cache[key]
+    return memoized_function
 
-def build_valid_moves():
+def build_valid_moves(UPPER_BOUND):
+    """Builds a dictionary where each key is a 2x2 matrix [a,b],[c,d] encoded as string 'abcd', and each value is the list of all possible moves from that state.
+
+    """
     move_list = dict()
     state_list = dict()
-    counter = set()
     for i1 in range(UPPER_BOUND):
         for i2 in range(UPPER_BOUND):
             for j1 in range(UPPER_BOUND):
@@ -38,29 +48,84 @@ def build_valid_moves():
                     if str_sum_of_row_cols not in move_list.keys():
                         move_list[str_sum_of_row_cols] = list()
                     move_list[str_sum_of_row_cols].append(str_cells)
-                    counter.add(str_cells)
                     if str_cells not in state_list:
                         state_list[str_cells] = move_list[str_sum_of_row_cols]
     return {state: [substate for substate in state_list[state] if state != substate] for state in state_list.keys() if len(state_list[state]) > 1}
-    #print([state for state in state_list.keys() if len(state_list[state]) > 1])
-    #return [list(state_list[state]) for state in state_list.items() if state_list[state] > 1]
 
-    #print(len(move_list))
-    #print(move_list)
+@memoize
+def get_2_by_2_score(a,b,c,d):
+    return (abs((a-c) * (b-d))/ (a + b + c + d)) if (a + b + c + d) != 0 else 0
+    #print(a,b,c,d)
+    #print((abs(a + d - b - c) / (a + b + c + d)) if (a + b + c + d) != 0 else 0)
+    #return (abs(a + d - b - c) / (a + b + c + d)) if (a + b + c + d) != 0 else 0
 
+def get_ast_2_by_2_score(a,b,c,d):
+    return ((a - c) ** 2 + (b - d) ** 2) / (a ** 2 + b ** 2 + c ** 2 + d ** 2) if (a+b+c+d) != 0 else 0
 
+def get_ast_score(mat, K):
+    score = 0
+    for row1 in range(len(mat)):
+        for row2 in range(row1 + 1, len(mat)):
+            for col1 in range(len(mat[0])):
+                for col2 in range(col1 + 1, len(mat[0])):
+                    score += get_ast_2_by_2_score(mat[row1][col1], mat[row1][col2], mat[row2][col1], mat[row2][col2])
+    return 4 * score / K
 
+@memoize
+def get_comp_score(mat, K):
+    score = 0
+    cu = 0
+    for row1 in range(len(mat)):
+        for row2 in range(row1 + 1, len(mat)):
+            for col1 in range(len(mat[0])):
+                for col2 in range(col1 + 1, len(mat[0])):
+                    #print(row1, row2, col1, col2)
+                    if (mat[row1][col1] == 1 and mat[row2][col2] == 1 and mat[row1][col2] == 0 and mat[row2][col1] == 0):
+                        cu += 1
+                        #print('checkerboard')
+                    if (mat[row1][col1] == 0 and mat[row2][col2] == 0 and mat[row1][col2] == 1 and mat[row2][col1] == 1):
+                        cu += 1
+                        #print('checkerboard')
+                    #print(abs(mat[row1][col1] + mat[row2][col2] - mat[row1][col2] - mat[row2][col1]))
+                    #print(mat[row1][col1], mat[row2][col2],  mat[row1][col2], mat[row2][col1])
+                    score += get_2_by_2_score(mat[row1][col1], mat[row1][col2], mat[row2][col1], mat[row2][col2])
+                    #score += abs(mat[row1][col1] + mat[row2][col2] - mat[row1][col2] - mat[row2][col1])
+    #print('CU: ', cu)
+    #print('K: ', K)
+    return score / K
+    #return cu
 
+@memoize
+def count_2x2_degree(a,b,c,d):
+    col_sum_1 = a + c
+    col_sum_2 = b + d
+    row_sum_1 = a + b 
+    row_sum_2 = c + d
+    return min(col_sum_1,col_sum_2,row_sum_1,row_sum_2)
 
-def swap_new(mat, move_list, log = False):
+@memoize
+def count_degree_fast(mat):
+    count = 0
+    for row1 in range(len(mat)):
+        for row2 in range(row1, len(mat)):
+            if row1 != row2:
+                for col1 in range(len(mat[0])):
+                    for col2 in range(col1, len(mat[0])):
+                        if col1 != col2:
+                            count += count_2x2_degree(mat[row1][col1], mat[row1][col2], mat[row2][col1], mat[row2][col2])
+                            #print('2STATE IS ',str(mat[row1][col1]) + str(mat[row1][col2])  + str(mat[row2][col1]) + str(mat[row2][col2]), ' DEGREE IS ',count_2x2_degree(mat[row1][col1], mat[row1][col2], mat[row2][col1], mat[row2][col2]))
+                            #count += max(mat[row1][col1],mat[row1][col2],mat[row2][col1],mat[row2][col2]) - 1
+    return count
+
+def swap_new(old_mat, move_list, log = False):
+    mat = deepcopy(old_mat)
     row_indexes = sample(list(range(0, len(mat))),2) 
     col_indexes = sample(list(range(0, len(mat[0]))),2) 
     i1, i2 = mat[row_indexes[0]][col_indexes[0]], mat[row_indexes[0]][col_indexes[1]]        #mat[0][0], mat[0], [1]
     j1,j2 = mat[row_indexes[1]][col_indexes[0]], mat[row_indexes[1]][col_indexes[1]]
     cur_state = str(i1) + str(i2) + str(j1) + str(j2)
-    print(cur_state)
     if cur_state not in move_list:
-        return mat
+        return None
     selected_move_index = randint(0, len(move_list[cur_state]) - 1)
     new_move_str = move_list[cur_state][selected_move_index]
     new_val_i1, new_val_i2, new_val_j1, new_val_j2 = int(new_move_str[0]),int(new_move_str[1]), int(new_move_str[2]), int(new_move_str[3])
@@ -70,83 +135,221 @@ def swap_new(mat, move_list, log = False):
     mat[row_indexes[1]][col_indexes[1]] = new_val_j2
     return mat
 
+def swap_no_precompute(old_mat):
+    row_indexes = sample(list(range(0, len(old_mat))),2) 
+    col_indexes = sample(list(range(0, len(old_mat[0]))),2) 
+    i1, i2 = old_mat[row_indexes[0]][col_indexes[0]], old_mat[row_indexes[0]][col_indexes[1]]        #mat[0][0], mat[0], [1]
+    j1,j2 = old_mat[row_indexes[1]][col_indexes[0]], old_mat[row_indexes[1]][col_indexes[1]]
+    row_sum_1, row_sum_2, col_sum_1, col_sum_2 = i1 + i2, j1 + j2, i1 + j1, i2 + j2
+    max_val = min(row_sum_1, col_sum_1)
+    #print('MAX VAL IS ', max_val)
+    random_i = randint(0, max_val)
+    new_i1 = random_i
+    new_i2 = row_sum_1 - new_i1
+    new_j1 = col_sum_1 - new_i1
+    new_j2 = row_sum_2 - new_j1
+    if new_i1 < 0 or new_j1 < 0 or new_i2 < 0 or new_j2 < 0:
+        return None
+
+    mat = deepcopy(old_mat)    
+    mat[row_indexes[0]][col_indexes[0]] = new_i1
+    mat[row_indexes[0]][col_indexes[1]] = new_i2
+    mat[row_indexes[1]][col_indexes[0]] = new_j1
+    mat[row_indexes[1]][col_indexes[1]] = new_j2
+    #print('CHANGED ',i1,i2, j1, j2, 'TO',new_i1, new_i2, new_j1, new_j2)
+    return mat
 
 
 
-def swap(mat, log = False):
-    """selects two random rows and cols. Tests if the swap on these is valid, swaps if it is. """
-    #sample two rows/cols no replacement
-    row_indexes = sample(list(range(0, len(mat))),2) 
-    col_indexes = sample(list(range(0, len(mat[0]))),2) 
-    i1, i2 = mat[row_indexes[0]][col_indexes[0]], mat[row_indexes[0]][col_indexes[1]]        #mat[0][0], mat[0], [1]
-    j1,j2 = mat[row_indexes[1]][col_indexes[0]], mat[row_indexes[1]][col_indexes[1]]
-    
-    if log:
-            print('VALS', i1, i2, j1, j2)
-            print('INDEXES',row_indexes, col_indexes)
-            print(mat)
-    #We have either (0,0),(0,0), or (1,1),(1,1). If it is (1,1),(1,1), we can swap to (2,0),(0,2)
-    if i1 == j1 and i2 == j2 and i1 == i2:
-        if i1 + j1 < UPPER_BOUND:
-            mat[row_indexes[0]][col_indexes[0]] = i1 + j1
-            mat[row_indexes[1]][col_indexes[1]] = i1 + j1
-            mat[row_indexes[0]][col_indexes[1]] -= j1
-            mat[row_indexes[1]][col_indexes[0]] -= j2
-        return mat
 
-    #We have (a,b),(c,d) with a+b = c+d. In this case, swap (a,b) and (c,d). This could be (0,1) and (1,0), or (1,1) and (2,0), or (2,1) and (1,2), or (2,0) and (0,2)
-    if i1 + i2 == j1 + j2:
-        if log:
-            print(i1, i2, j1, j2)
-            print(row_indexes, col_indexes)
-            print(mat)
-        if(i1 == 2 and i2 == 0 and j1 == 0 and j2 == 2):
-            mat[row_indexes[0]][col_indexes[0]] = 1
-            mat[row_indexes[1]][col_indexes[1]] = 1
-            mat[row_indexes[0]][col_indexes[1]] = 1
-            mat[row_indexes[1]][col_indexes[0]] = 1
-        else:
-            mat[row_indexes[0]][col_indexes[0]],mat[row_indexes[1]][col_indexes[0]]  = mat[row_indexes[1]][col_indexes[0]], mat[row_indexes[0]][col_indexes[0]]
-            mat[row_indexes[0]][col_indexes[1]], mat[row_indexes[1]][col_indexes[1]] = mat[row_indexes[1]][col_indexes[1]], mat[row_indexes[0]][col_indexes[1]]
+@memoize
+def count_degree(mat, move_list):
+    count = 0
+    for row1 in range(len(mat)):
+        for row2 in range(row1, len(mat)):
+            if row1 != row2:
+                for col1 in range(len(mat[0])):
+                    for col2 in range(col1, len(mat[0])):
+                        if col1 != col2:
+                            state = str(mat[row1][col1]) + str(mat[row1][col2])  + str(mat[row2][col1]) + str(mat[row2][col2])
+                            if state in move_list:
+                                count += len(move_list[state])
+                                #print('STATE IS ', state, ' DEGREE IS ', len(move_list[state]))
+                            #else: 
+                            #    print(state)
+    return count
 
-    #We have (a,b),(c,d) with a+b = c+d. In this case, swap (a,b) and (c,d). This could be (0,1) and (1,0), or (1,1) and (2,0), or (2,1) and (1,2), or (2,0) and (0,2)
-    elif i1 + j1 == i2 + j2:
-        if(i1 == 2 and i2 == 0 and j1 == 0 and j2 == 2):
-            mat[row_indexes[0]][col_indexes[0]] = 1
-            mat[row_indexes[1]][col_indexes[1]] = 1
-            mat[row_indexes[0]][col_indexes[1]] = 1
-            mat[row_indexes[1]][col_indexes[0]] = 1
-        mat[row_indexes[0]][col_indexes[0]],mat[row_indexes[0]][col_indexes[1]]  = mat[row_indexes[0]][col_indexes[1]], mat[row_indexes[0]][col_indexes[0]]
-        mat[row_indexes[1]][col_indexes[0]], mat[row_indexes[1]][col_indexes[1]] = mat[row_indexes[1]][col_indexes[1]], mat[row_indexes[1]][col_indexes[0]]
-            
+def graph_scores(scores, nbins=10):
+    fig, ax = plt.subplots()
+    n, bins, patches = ax.hist(scores, nbins)
+    ax.set(xlabel="Checkerboard sum", ylabel="Frequency", title="Distribution of checkerboard sums")
+    #ax.scatter(scores, cu)
+    fig.savefig('dist.png')
+    plt.show()
 
-def test_swap_mat(seed, move_list, log = False):
-    matrices = set()
-    counter = {0:0, 1:0, 2:0}
-    print('SEED: ',seed)
-    for i in range(10000):
-        #count = sum([row.count(2) for row in seed])
-        #counter[count] += 1
-        #print(counter)
-        swap_new(seed, move_list, log)
-        matrices.add(tuple(tuple(i) for i in seed))
-    [print(m) for m in matrices]
+
+def fast_random_walk(seed, n):
+    matrices = list()
+    cur_mat = seed
+    i = 0
+    while i < BURN_IN_TIME + n:
+        swap_result = swap_no_precompute(cur_mat)
+        if type(swap_result) != np.ndarray and swap_result == None:
+            continue
+        i += 1
+        degree_old, degree_new = count_degree_fast(cur_mat), count_degree_fast(swap_result)
+        if degree_old == 0:
+            print(cur_mat)
+            exit()
+        if degree_new == 0:
+            print(swap_result)
+            exit()
+        rand_number = uniform(0,1)
+        if rand_number < min(1,degree_old / degree_new):
+            cur_mat = swap_result
+        if i > BURN_IN_TIME:
+            matrices.append(cur_mat)
+    return cur_mat if n == 1 else matrices[-1 * n:]
+
+
+
+def random_walk(seed, move_list, n, log = False):
+    matrices = list()
+    cur_mat = seed
+    i = 0
+    while i < BURN_IN_TIME + n:
+        #swap_result = swap_new(cur_mat, move_list, log)
+        swap_result = swap_no_precompute(cur_mat)
+        if type(swap_result) != np.ndarray and swap_result == None:
+            continue
+        i += 1
+        degree_old, degree_new = count_degree_fast(cur_mat), count_degree_fast(swap_result)
+        rand_number = uniform(0,1)
+        if rand_number <= min(1,degree_old / degree_new):
+            cur_mat = swap_result
+        if i > BURN_IN_TIME:
+            matrices.append(cur_mat)
+    return cur_mat if n == 1 else matrices[-1 * n:]
+
+def get_sample(seed, move_list):
+    sample = list()
+    if USE_INDEPENDENT_SAMPLES:
+        n = 1
+        for _ in range(SAMPLE_SIZE):
+            if _ % 10 == 0:
+                print(_)
+            sample.append(random_walk(seed, move_list, n))
+    else:
+        sample = random_walk(seed, move_list, SAMPLE_SIZE)
+    return sample
+
+def get_scores(matrices, score_fn):
+    mat = matrices[0]
+    K = (len(mat) * (len(mat) - 1) * len(mat[0]) * (len(mat[0]) - 1))
+    return [score_fn(m, K) for m in matrices]
+
+def get_prop(score, matrices, score_fn):
+    #print(matrices)
+    scores = get_scores(matrices, score_fn)
+    return len([s for s in scores if s >= score]) / len(scores)
+
+def find_upper_bound(A):
+    max_row = max([sum(A[i]) for i in range(len(A))])
+    col_sums = [0] * len(A[0])
+    for r in A:
+        for i in range(len(r)):
+            col_sums[i] += r[i]
+    max_col = max(col_sums)
+    return max(max_row, max_col)
+
+def get_mat_list(filename = 'Matrices.txt', cast_to_int = True):
+    matrices = dict()
+    has_num_re = re.compile('\d')
+    with open(filename, 'r') as f:
+        cur_mat = []
+        title = ''
+        for line in f:
+            if line.isspace():
+                matrices[title] = cur_mat
+                cur_mat = []
+                title = ''
+                continue
+            if not has_num_re.match(line):
+                if title == '':
+                        title = line.split()[0][:-1]
+                continue
+            if cast_to_int:
+                split_line_nums = [int(float(n)) for n in line.split()[1:]]
+            else:
+                split_line_nums = [float(n) for n in line.split()[1:]]
+            cur_mat.append(split_line_nums)
     return matrices
 
+def print_mat_dim(test_mat_list):
+    for name,m in test_mat_list.items():
+        if name == '':
+            continue
+        if type(m) == list:
+                print(name)
+                print(len(m),'x',len(m[0]))
 
-#all_mat = gen_all_mat()
-#while len(res) < 31:
-#    res = gen_all_mat()
-#res2 = test_swap_mat(sample([list([list(r) for r in l]) for l in res], 1)[0])
-#print(list(range(0,5)))
-#res2 = test_swap_mat([[1,1,1],[1,1,1],[1,1,1]])
-#diff = res - res2
-#res3 = test_swap_mat(list([list(r) for r in list(diff)[2]]), True)
-#print(len(res3))
-#[print(r) for r in res3]
-moves = build_valid_moves()
-#print(moves)
-res = test_swap_mat([[2,2,2],[0,1,0],[0,0,0]], moves, True)
-#print(len(res))
-#sprint(len(all_mat - res))
+def run_sim(test_mat, name):
+    start = timer()
+    print('RUNNING ',name)
+    outfile = open('output_files/' + name + '.txt', 'w')
+    K = (len(test_mat) * (len(test_mat) - 1) * len(test_mat[0]) * (len(test_mat[0]) - 1))
+    outfile.write('\nDIMENSIONS: ' + str(len(test_mat)) + 'x' + str(len(test_mat[0])))
+    outfile.write('\nTEST SCORE CS: ' + str(get_comp_score(test_mat, K)))
+    outfile.write('\nTEST SCORE AST: ' + str(get_ast_score(test_mat, K)))
+    sample = get_sample(test_mat, [])
+    scores = get_scores(sample, get_comp_score)
+    print(sorted(scores))
+    #p = [get_prop(s, sample, get_comp_score) for s in scores]
+    #outfile.write(sorted(p))
+    #graph_scores(scores)
+    
+    p = get_prop(get_comp_score(test_mat, K), sample, get_comp_score)
+    outfile.write('\nPROPORTION CS: ' +  str(p))
+    p = get_prop(get_ast_score(test_mat, K), sample, get_ast_score)
+    outfile.write('\nPROPORTION AST: ' + str(p))
+    end = timer()
+    outfile.write('\nTOOK ' + str(end - start) +' SECONDS')
+    outfile.close()
+    print('FINISHED ', name)
 
+def run():
+    test_mat_list = get_mat_list()
+    mat_names = sorted(test_mat_list.keys())
+    print(mat_names)
+    start_name = ''
+    if start_name == '':
+        start_name = mat_names[0]
+    mat_names = mat_names[mat_names.index(start_name):]
+    mat_list = set()
+    args = list()
+    count = 0
+    for m in mat_names:
+        if m == '':
+            continue
+        count += 1
+        args.append(tuple([test_mat_list[m],m]))
+    start = timer()
+    #while len(args) > 0:
+    #    cur_args = list()
+    #    for _ in range(10):
+    #        cur_args.append(args.pop())
+    #    with Pool(10) as p:
+    #        #print(p)
+    #        p.starmap(run_sim, cur_args)
+    with Pool() as p:
+        p.starmap(run_sim, args)
+    end = timer()
+    print('TOOK ', end - start, ' SECONDS')
+
+def run_single():
+    test_mat_list = get_mat_list()
+    run_sim(test_mat_list['Egg83'], 'Egg83')
+
+if __name__ == "__main__":
+    #run()
+    run_single()
